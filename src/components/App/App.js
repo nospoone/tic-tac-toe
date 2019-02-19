04bab0e38a3e0a6a9e7dfda05bb505157ce89c9e
Roméo Jr. Labonté <romeo@q-bit.ca>
2019-02-18 23:54:15 -0500

    title with passing tests

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
 
+import Title from '../Title/Title';
+
 class App extends Component {
 	render() {
 		return (
-			<div/>
+			<Title/>
 		);
 	}
 }
---
+++ src/components/Title/Title.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './Title.css';
+
+export default class Title extends Component {
+	render() {
+		return (
+			<h1 className="title">Noughts &amp; Crosses</h1>
+		);
+	}
+}

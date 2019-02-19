4dfb4f4b94276504e9e77d586f249cf72987b98f
Roméo Jr. Labonté <romeo@q-bit.ca>
2019-02-19 08:14:46 -0500

    square w/ passing tests

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,17 @@ import React, {Component} from 'react';
 import './App.css';
 
 import Title from '../Title/Title';
+import Square from '../Square/Square';
 
 class App extends Component {
 	render() {
 		return (
-			<Title/>
+			<>
+				<Title/>
+				<Square mark="o"/>
+				<Square mark="x"/>
+				<Square/>
+			</>
 		);
 	}
 }

1e06cb8027ea30214c0046404b9769be1c2afc7a
Roméo Jr. Labonté <romeo@q-bit.ca>
2019-02-18 22:30:05 -0500

    moved test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import App from '../components/App/App';
+import App from './App';
 
 describe('App', () => {
 	it('should render a <div />', () => {

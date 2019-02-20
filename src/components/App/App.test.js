c8962bba91ff87a37b5e4bcdacb4af32014fbb59
Roméo Jr. Labonté <romeo@q-bit.ca>
2019-02-20 15:23:55 -0500

    app structure failing tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {shallow} from 'enzyme';
 
 import App from './App';
 import Title from '../Title/Title';
+import Square from '../Square/Square';
 
 describe('App', () => {
-	it('should render a <div />', () => {
+	it('should render a <Fragment /> as its root', () => {
 		const wrapper = shallow(<App/>);
-		expect(wrapper.find(Title).length).toEqual(1);
+		expect(wrapper.at(0).is(Fragment)).toBe(true);
+	});
+	it('should contain a <Title/>', () => {
+		const wrapper = shallow(<App/>);
+		expect(wrapper.find(Title).length).toBe(1);
+	});
+	// HTML structure (rows w/ squares)
+	it('should contain three <div/> with the `row` class', () => {
+		const wrapper = shallow(<App/>);
+		expect(wrapper.find('div.row').length).toBe(3);
+	});
+	it('should contain nine <Square/>s inside three <div/> with the `row` class', () => {
+		const wrapper = shallow(<App/>);
+		expect(wrapper.find('div.row').length).toBe(3);
+		wrapper.find('div.row').forEach(row => {
+			expect(row.find(Square).length).toBe(3);
+		});
 	});
 });

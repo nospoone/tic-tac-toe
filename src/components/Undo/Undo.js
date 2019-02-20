6c69bbe99453f0e5da062eea7aba9e953865c7ef
Roméo Jr. Labonté <romeo@q-bit.ca>
2019-02-20 17:03:14 -0500

    undo failing tests

---
+++ src/components/Undo/Undo.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import './Undo.css';
+
+export default class Title extends Component {
+	render() {
+		return undefined;
+	}
+}
---
+++ src/components/Undo/Undo.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Undo from './Undo';
+
+describe('Undo', () => {
+	it('should render a <span/> with an `undo` class', () => {
+		const wrapper = shallow(<Undo/>);
+		expect(wrapper.find('span.undo').length).toEqual(1);
+	});
+	it('should have `Undo last move` as its text', () => {
+		const wrapper = shallow(<Undo/>);
+		expect(wrapper.find('span.undo').text()).toEqual('Undo last move');
+	});
+	it('should have the `undo--disabled` class when the `disabled` prop is set', () => {
+		const wrapper = shallow(<Undo/>);
+		expect(wrapper.find('span.undo--disabled').length).toEqual(1);
+	});
+});

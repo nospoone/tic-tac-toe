b438587e54d8e1aa8da0414e20f571abb30fd831
Roméo Jr. Labonté <romeo@q-bit.ca>
2019-02-20 23:11:37 -0500

    storeMove implementation

--- src/logic/game.js
+++ src/logic/game.js
@@ -1,1 +1,23 @@
-export class Game {}
+export default class Game {
+	constructor() {
+		this.emptyBoard = [
+			[null, null, null],
+			[null, null, null],
+			[null, null, null]
+		];
+
+		this.board = this.emptyBoard;
+	}
+
+	storeMove(player, x, y) {
+		if (x > 2 || y > 2) {
+			throw new Error('Move position out of bounds.');
+		}
+
+		if (player !== 'x' && player !== 'o') {
+			throw new Error('Invalid player.');
+		}
+
+		this.board[y][x] = player;
+	}
+}

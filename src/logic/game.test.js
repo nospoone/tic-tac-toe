ad9127051b268f805a1f785604f6cff09af7c9fe
Roméo Jr. Labonté <romeo@q-bit.ca>
2019-02-21 00:25:37 -0500

    test restructure + failing deserialize tests

--- src/logic/game.test.js
+++ src/logic/game.test.js
@@ -1,42 +1,52 @@
 import Game from './game';
 
 describe('Game', () => {
-	it('should store moves correctly', () => {
-		const game = new Game();
-		game.storeMove('x', 0, 0);
-		expect(game.board[0][0]).toBe('x');
-		game.storeMove('o', 2, 1);
-		expect(game.board[1][2]).toBe('o');
+	let game;
+
+	beforeEach(() => {
+		game = new Game();
 	});
-	it('should not store invalid moves', () => {
-		const game = new Game();
-		expect(() => {
-			game.storeMove('y', 0, 0);
-		}).toThrow('Invalid player.');
-		expect(() => {
-			game.storeMove('o', 13, 21);
-		}).toThrow('Move position out of bounds.');
+
+	describe('Moves', () => {
+		it('should store moves correctly', () => {
+			game.storeMove('x', 0, 0);
+			expect(game.board[0][0]).toBe('x');
+			game.storeMove('o', 2, 1);
+			expect(game.board[1][2]).toBe('o');
+		});
+		it('should not store invalid moves', () => {
+			expect(() => {
+				game.storeMove('y', 0, 0);
+			}).toThrow('Invalid player.');
+			expect(() => {
+				game.storeMove('o', 13, 21);
+			}).toThrow('Move position out of bounds.');
+		});
 	});
-	it('should correctly serialize the board state', () => {
-		const game = new Game();
+
+	describe('Serialization', () => {
 		const board = [
 			['x', null, 'o'],
 			[null, 'x', 'o'],
 			[null, null, 'x']
 		];
-		game.board = board;
-		const serializedBoard = game.serialize();
-		expect(serializedBoard).toBe(JSON.stringify(board));
-	});
-	it('should correctly serialize the board state to localStorage', () => {
-		const game = new Game();
-		const board = [
-			['x', null, 'o'],
-			[null, 'x', 'o'],
-			[null, null, 'x']
-		];
-		game.board = board;
-		game.serialize();
-		expect(localStorage.__STORE__[game.boardHistoryKey]).toBe(JSON.stringify(board));
+		const serializedBoard = JSON.stringify(board);
+
+		beforeEach(() => {
+			game.board = board;
+		});
+
+		it('should correctly serialize the board state', () => {
+			const serializedBoard = game.serialize();
+			expect(serializedBoard).toBe(serializedBoard);
+		});
+		it('should correctly serialize the board state to localStorage', () => {
+			game.serialize();
+			expect(localStorage.__STORE__[game.boardHistoryKey]).toBe(serializedBoard);
+		});
+		it('should correctly deserialize the board state from localStorage', () => {
+			game.deserialize();
+			expect(game.board).toBe(board);
+		});
 	});
 });

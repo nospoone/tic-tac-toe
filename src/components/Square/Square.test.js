f9af58ea518de9c0bada9331e968f4b5ce5ee9e0
Roméo Jr. Labonté <romeo@q-bit.ca>
2019-02-21 21:58:08 -0500

    add square onclick failing tests

--- src/components/Square/Square.test.js
+++ src/components/Square/Square.test.js
@@ -6,39 +6,74 @@ import Mark from '../Mark/Mark';
 
 describe('Square', () => {
 	it('should render a <div />', () => {
-		const wrapper = shallow(<Square/>);
+		const wrapper = shallow(<Square x={0} y={0} onClick={() => {}}/>);
 		expect(wrapper.find('div').length).toEqual(1);
 	});
 	it('should render a <div /> with the `square--pulse` class when the `pulse` prop is set', () => {
-		const wrapper = shallow(<Square pulse/>);
+		const wrapper = shallow(<Square pulse x={0} y={0} onClick={() => {}}/>);
 		expect(wrapper.find('div.square--pulse').length).toEqual(1);
 	});
 	it('should render a <div /> with the `square--disabled` when the `mark` prop is set', () => {
-		const wrapper = shallow(<Square mark="x"/>);
+		const wrapper = shallow(<Square x={0} y={0} mark="x" onClick={() => {}}/>);
 		expect(wrapper.find('div.square--disabled').length).toEqual(1);
 	});
 	it('should render a <Mark /> with its `mark` prop set to `x` when the `mark` prop is `x`', () => {
-		const wrapper = shallow(<Square mark="x"/>);
+		const wrapper = shallow(<Square x={0} y={0} mark="x" onClick={() => {}}/>);
 		expect(wrapper.find(Mark).length).toEqual(1);
 		expect(wrapper.find(Mark).prop('mark')).toEqual('x');
 		expect(wrapper.find(Mark).prop('active')).toEqual(true);
 	});
 	it('should render a <Mark /> with its `mark` prop set to `x` when the `mark` prop is `x`', () => {
-		const wrapper = shallow(<Square mark="o"/>);
+		const wrapper = shallow(<Square x={0} y={0} mark="o" onClick={() => {}}/>);
 		expect(wrapper.find(Mark).length).toEqual(1);
 		expect(wrapper.find(Mark).prop('mark')).toEqual('o');
 		expect(wrapper.find(Mark).prop('active')).toEqual(true);
 	});
 	it('should render a <Mark /> with its `active` prop set to `true` when the `mark` prop is not `undefined`', () => {
-		const wrapper = shallow(<Square mark="o"/>);
+		const wrapper = shallow(<Square x={0} y={0} mark="o" onClick={() => {}}/>);
 		expect(wrapper.find(Mark).length).toEqual(1);
 		expect(wrapper.find(Mark).prop('mark')).toEqual('o');
 		expect(wrapper.find(Mark).prop('active')).toEqual(true);
 	});
 	it('should render a <Mark /> when the `mark` prop is `undefined`', () => {
-		const wrapper = shallow(<Square/>);
+		const wrapper = shallow(<Square x={0} y={0} onClick={() => {}}/>);
 		expect(wrapper.find(Mark).length).toEqual(1);
 		expect(wrapper.find(Mark).prop('active')).toEqual(false);
 		expect(wrapper.find(Mark).prop('mark')).toEqual(undefined);
+	});
+	it('should log a warning when the `x` prop is invalid', () => {
+		const spy = jest.spyOn(global.console, 'error').mockImplementation(() => {});
+		shallow(<Square x={4} y={0} onClick={() => {}}/>);
+		expect(spy).toHaveBeenCalled();
+		expect(spy.mock.calls[0][0]).toContain('Invalid prop `x` of value `4` supplied to `Square`');
+		spy.mockClear();
+	});
+	it('should log a warning when the `y` prop is invalid', () => {
+		const spy = jest.spyOn(global.console, 'error').mockImplementation(() => {});
+		shallow(<Square x={0} y={6} onClick={() => {}}/>);
+		expect(spy).toHaveBeenCalled();
+		expect(spy.mock.calls[0][0]).toContain('Invalid prop `y` of value `6` supplied to `Square`');
+		spy.mockClear();
+	});
+	it('should log a warning when the `onClick` prop is invalid', () => {
+		const spy = jest.spyOn(global.console, 'error').mockImplementation(() => {});
+		shallow(<Square x={0} y={0} onClick="invalid"/>);
+		expect(spy).toHaveBeenCalled();
+		expect(spy.mock.calls[0][0]).toContain('Invalid prop `onClick` of type `string` supplied to `Square`');
+		spy.mockClear();
+	});
+	it('should log a warning when the `onClick` prop is missing', () => {
+		const spy = jest.spyOn(global.console, 'error').mockImplementation(() => {});
+		shallow(<Square x={0} y={0}/>);
+		expect(spy).toHaveBeenCalled();
+		expect(spy.mock.calls[0][0]).toContain('The prop `onClick` is marked as required in `Square`, but its value is `undefined`');
+		spy.mockClear();
+	});
+	it('should call the `onClick` prop with the `x, y` parameters when the component is clicked', () => {
+		const handleClickMock = jest.fn(() => {});
+		const wrapper = shallow(<Square x={0} y={1} onClick={handleClickMock}/>);
+		wrapper.simulate('click');
+		expect(handleClickMock).toHaveBeenCalled();
+		expect(handleClickMock).toHaveBeenCalledWith(0, 1);
 	});
 });

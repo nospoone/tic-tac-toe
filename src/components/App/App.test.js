ceb5dbe5dc98267b3cba66e80520e4ef6e07df2a
Roméo Jr. Labonté <romeo@q-bit.ca>
2019-02-22 23:11:24 -0500

    removed wrong test case

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -188,9 +188,6 @@ describe('App', () => {
 		wrapper.find('div.row[data-row=1]').childAt(1).simulate('click');
 		wrapper.update();
 		expect(wrapper.find('div.status').find(TurnIndicator).prop('player')).toBe('x');
-		wrapper.find('div.row[data-row=0]').childAt(2).simulate('click');
-		wrapper.update();
-		expect(wrapper.find('div.status').find(TurnIndicator).prop('player')).toBe('o');
 	});
 	it('should set the `player` prop on <TurnIndicator/> correctly when the game is finished', () => {
 		const wrapper = mount(<App/>);
